60d6ea8f47aa24e0325b0589f2a9816e30d4a8c0
Kinley L <mckinleylaberge@gmail.com>
2025-03-11 13:23:10 -0500

    Quartz sync: Mar 11, 2025, 1:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Kinley's Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
