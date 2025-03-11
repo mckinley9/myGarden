a85d431b3c74e493d42fdb50a26a7093e72379a3
Kinley L <mckinleylaberge@gmail.com>
2025-03-11 13:53:35 -0500

    Quartz sync: Mar 11, 2025, 1:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -31,7 +31,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           light: "#eeeeee",
-          lightgray: "#706f5a",
+          lightgray: "#e6e6e6",
           gray: "#62624e",
           darkgray: "#2f2f25",
           dark: "#23231c",
